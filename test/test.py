0d374fd4b6d274c1e1dea056d1baa17c24e51e7f
itsuki-jp <itsuki19601@gmail.com>
2022-02-15 21:37:03 +0900

    ABC 082 C

--- test/test.py
+++ test/test.py
@@ -1,16 +1,14 @@
-for n in range(1, 20):
-    l = len(str(n))
-    mod = 998244353
-    past = 0
-    nine = 9
-    past_nine = 0
-    for i in range(l):
-        if i != (l - 1):
-            past += (1 + nine - past_nine) * (nine - past_nine) / 2
-            past_nine = nine
-            nine *= 10
-            nine += 9
-        else:
-            ans = past + (1 + n - past_nine) * (n - past_nine) / 2
-            print(int(ans) % mod)
-print("gue-")
+from collections import Counter
+
+n = int(input())
+a = list(map(int, input().split()))
+
+cnt = Counter(a)
+ans = 0
+for k in cnt.keys():
+    num = cnt[k]
+    if num >= k:
+        ans += num - k
+    else:
+        ans += num
+print(ans)
