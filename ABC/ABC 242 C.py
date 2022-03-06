21e15aca72be70cf34dea3f481d60da50120ff61
itsuki-jp <itsuki19601@gmail.com>
2022-03-06 22:10:08 +0900

    ABC 242　C きれいにした

--- ABC/ABC 242 C.py
+++ ABC/ABC 242 C.py
@@ -1,21 +1,17 @@
 mod = 998244353
 n = int(input())
-lst = [2, 3, 3, 3, 3, 3, 3, 3, 2]
-if n == 2:
-    exit(print(25))
+lst = [1 for _ in range(9)]
 
-for i in range(n - 2):
+# 791ms
+for i in range(n - 1):
     n_lst = [0 for _ in range(9)]
-    n_lst[0] = lst[0] + lst[1]
-    n_lst[1] = sum(lst[:3])
-    n_lst[2] = sum(lst[1:4])
-    n_lst[3] = sum(lst[2:5])
-    n_lst[4] = sum(lst[3:6])
-    n_lst[5] = sum(lst[4:7])
-    n_lst[6] = sum(lst[5:8])
-    n_lst[7] = sum(lst[6:9])
-    n_lst[8] = lst[7] + lst[8]
+    for j in range(9):
+        if j == 0:
+            n_lst[0] = lst[0] + lst[1]
+        elif j == 8:
+            n_lst[8] = lst[7] + lst[8]
+        else:
+            n_lst[j] = sum(lst[j - 1:j + 2])
+
     lst = [n_lst[_] % mod for _ in range(9)]
 print(sum(lst) % mod)
-
-
