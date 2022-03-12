7b4b96ecf9a486596be8a84edac975ecb3ead3c5
itsuki-jp <itsuki19601@gmail.com>
2022-03-12 23:40:29 +0900

    解説AC

--- ABC/ABC 243 E.py
+++ ABC/ABC 243 E.py
@@ -1,3 +1,4 @@
+# 解説AC
 # warshall_floyd法
 def warshall_floyd():
     for k in range(n):
@@ -5,7 +6,6 @@ def warshall_floyd():
             for j in range(n):
                 if d[i][k] + d[k][j] < d[i][j]:
                     d[i][j] = d[i][k] + d[k][j]
-                    prev[i][j] = prev[k][j]
 
 
 # n:頂点数　m:辺の数
@@ -14,24 +14,21 @@ n, m = map(int, input().split())
 # 隣接行列の定義、初期化
 # ①コスト(存在しないときはinf)
 inf = float("inf")
-d = [[inf for _ in range(n + 1)] for _ in range(n + 1)]
-
-# ②自分自身へのコストは０
-for i in range(n):
-    d[i][i] = 0
+d = [[inf for _ in range(n)] for _ in range(n)]
 
 # コスト入力（何もないときは１，問題によっては入力時に指定される）
+abc = []
 for i in range(m):
     a, b, c = map(int, input().split())
     d[a - 1][b - 1] = c
     d[b - 1][a - 1] = c
-prev = [[_ for __ in range(n + 1)] for _ in range(n + 1)]
+    abc.append((a - 1, b - 1, c))
 warshall_floyd()
-ans = set()
-for start in range(n - 1):
-    for goal in range(start + 1, n):
-        cur = goal
-        while cur != start:
-            ans.add((max(cur, prev[start][cur]), min(cur, prev[start][cur])))
-            cur = prev[start][cur]
-print(m - len(ans))
+ans = 0
+for s_a, s_b, s_c in abc:
+    temp = 0
+    for i in range(n):
+        if d[s_a][i] + d[i][s_b] <= s_c:
+            temp = 1
+    ans += temp
+print(ans)
