a86f4ff5ee3ac7810195bc89f1652d2440fa0302
itsuki-jp <itsuki19601@gmail.com>
2022-03-13 10:34:10 +0900

    ABC 243 D　解法1

--- ABC/ABC 243 D.py
+++ ABC/ABC 243 D.py
@@ -1,12 +1,36 @@
 # 解説AC
-n, x = map(int, input().split())
-s = input()
-b = list(bin(x)[2:])
-for i in s:
-    if i == "L":
-        b.append("0")
-    elif i == "R":
-        b.append("1")
-    else:
-        b.pop()
-print(int("".join(b), 2))
+def solve1():
+    n, x = map(int, input().split())
+    s = input()
+    b = list(bin(x)[2:])
+    for i in s:
+        if i == "L":
+            b.append("0")
+        elif i == "R":
+            b.append("1")
+        else:
+            b.pop()
+    print(int("".join(b), 2))
+
+
+def solve2():
+    n, x = map(int, input().split())
+    s = input()
+    lst = []
+    for i in s:
+        if len(lst) > 0 and lst[-1] in ("L", "R") and i == "U":
+            lst.pop()
+            continue
+        else:
+            lst.append(i)
+    for i in lst:
+        if i == "U":
+            x //= 2
+        elif i == "L":
+            x *= 2
+        else:
+            x = x * 2 + 1
+    print(x)
+
+
+solve2()
